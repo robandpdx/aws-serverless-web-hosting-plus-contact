4524c4d74d66011f227782ee0c7c0a2d82f93efb
Rob Anderson <robandpdx@github.com>
2022-05-09 23:27:15 -0500

    echo statements

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd web
+echo "Running npm install"
 npm install
+echo "Installing gulp"
 npm install -g gulp
+echo "Running gulp"
 gulp
+echo "Removing node_modules"
 rm -rf node_modules
 cd -
